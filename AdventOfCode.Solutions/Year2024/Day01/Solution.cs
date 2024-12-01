1e281ac1e31c4ea9f03298fbbb89aaaa4a581e95
Mitchell Barry <mitch.barry@gmail.com>
2024-12-01 16:26:51 +0000

    Added Day 1

--- AdventOfCode.Solutions/Year2024/Day01/Solution.cs
+++ AdventOfCode.Solutions/Year2024/Day01/Solution.cs
@@ -2,7 +2,7 @@ namespace AdventOfCode.Solutions.Year2024.Day01;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(01, 2024, "") { }
+    public Solution() : base(01, 2024, "Historian Hysteria", true) { }
 
     protected override string SolvePartOne()
     {
