e4dfd2dde8fe794e390de694d122292aba384bb1
Mitchell Barry <mitch.barry@gmail.com>
2024-12-03 05:17:05 +0000

    Day 3 solutions

--- AdventOfCode.Solutions/Year2024/Day03/Solution.cs
+++ AdventOfCode.Solutions/Year2024/Day03/Solution.cs
@@ -1,16 +1,50 @@
+using System.Text.RegularExpressions;
+
 namespace AdventOfCode.Solutions.Year2024.Day03;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(03, 2024, "") { }
+    private readonly Regex _regex = new Regex(@"(mul\((\d+),(\d+)\))", RegexOptions.Compiled);
+    public Solution() : base(03, 2024, "Mull It Over", false) { }
 
     protected override string SolvePartOne()
     {
-        return "";
+        var outputs = GetMultValues(Input);
+        return outputs.Sum().ToString();
     }
 
     protected override string SolvePartTwo()
     {
-        return "";
+        var instruction = string.Join("", Input).Trim();
+        while (instruction.IndexOf("don't()") > -1)
+        {
+            var firstDont = instruction.IndexOf("don't()");
+            var firstDo = instruction.IndexOf("do()", firstDont);
+            if (firstDo == -1)
+            {
+                instruction = instruction.Remove(firstDont);
+            }
+            else
+            {
+                instruction = instruction.Remove(firstDont, firstDo - firstDont + 4);
+            }
+        }
+        var outputs = GetMultValues(instruction);
+        return outputs.Sum().ToString();
+    }
+
+    private List<long> GetMultValues(string input)
+    {
+
+        var matches = _regex.Matches(string.Join("", input).Trim());
+        var outputs = new List<long>();
+        foreach (Match match in matches)
+        {
+            var groups = match.Groups;
+            var x = int.Parse(groups[2].Value);
+            var y = int.Parse(groups[3].Value);
+            outputs.Add(x * y);
+        }
+        return outputs;
     }
 }
