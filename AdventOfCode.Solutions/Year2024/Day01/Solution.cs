aac84ea437a6b27b9947af0cb54c452d83ad315b
Mitchell Barry <mitch.barry@gmail.com>
2024-12-03 01:32:23 +0000

    Day 1 solutions

--- AdventOfCode.Solutions/Year2024/Day01/Solution.cs
+++ AdventOfCode.Solutions/Year2024/Day01/Solution.cs
@@ -2,15 +2,41 @@ namespace AdventOfCode.Solutions.Year2024.Day01;
 
 class Solution : SolutionBase
 {
-    public Solution() : base(01, 2024, "Historian Hysteria", true) { }
+    List<long> leftSet = new List<long>();
+    List<long> rightSet = new List<long>();
+
+    public Solution() : base(01, 2024, "Historian Hysteria", false) 
+    {
+        var lines = input.SplitByNewline();
+        foreach (var line in lines)
+        {
+            var parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var left = long.Parse(parts[0]);
+            var right = long.Parse(parts[1]);
+            leftSet.Add(left);
+            rightSet.Add(right);
+        }
+    }
 
     protected override string SolvePartOne()
     {
-        return "";
+        var sortedLeft = leftSet.OrderBy(x => x).ToList();
+        var sortedRight = rightSet.OrderBy(x => x).ToList();
+        var diffs = new List<long>();
+        for (var i = 0; i < sortedLeft.Count; i++)
+        {
+            diffs.Add(Math.Abs(sortedLeft[i] - sortedRight[i]));
+        }
+        return diffs.Sum().ToString();
     }
 
     protected override string SolvePartTwo()
     {
-        return "";
+        long similarityScore = 0;
+        for (var i = 0; i < leftSet.Count; i++)
+        {
+            similarityScore += leftSet[i] * rightSet.Count(a => a == leftSet[i]);
+        }
+        return similarityScore.ToString();
     }
 }
